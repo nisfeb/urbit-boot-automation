95f5ca55387c5af4d053aef263827abe6d8e59b9
Jackson Egan <jackson@sneagan.com>
2021-02-16 21:59:20 -0500

    Add functional DO droplet creation

--- droplet_create.sh
+++ droplet_create.sh
@@ -6,7 +6,7 @@
 show_help() {
     echo "Usage: $0 [option...]" >&2
     echo
-    echo "   -n, --name      Ship name"
+    echo "   -n, --name      Ship name (without sig ~)"
     echo "   -t, --token     DigitalOcean token"
     echo
     exit 1
@@ -46,10 +46,12 @@ fi
 create_droplet() {
     # Generate SSH Key
     ssh-keygen -t rsa -b 2048 -f ./keys/$SHIP_NAME -C $SHIP_NAME -q -N ""
-    # Add SSH key to your DO account
-    key_response="$(curl -X POST -H 'Content-Type: application/json' -H 'Authorization: Bearer $DO_TOKEN' -d '{"name":"${SHIP_NAME} key","public_key":"$(cat /tmp/$SHIP_NAME.pub)"}' 'https://api.digitalocean.com/v2/account/keys')"
-    # Parse the SSH key ID from the HTTP reponse to the key creation call above
-
-    # Create a Droplet that will accept connections from the provided SSH key
-    curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $DO_TOKEN" -d '{"name":"${SHIP_NAME}","region":"nyc3","size":"s-2vcpu-4gb","image":"ubuntu-20-04-x64","ssh_keys":[$KEY_ID],"backups":false,"ipv6":true,"user_data":null,"private_networking":null,"volumes": null,"tags":["urbit"]}' "https://api.digitalocean.com/v2/droplets" 
+    # Add SSH key to your DO account and capture the reponse JSON
+    key_response=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $DO_TOKEN" -d "{\"name\":\"${SHIP_NAME} key\",\"public_key\":\"$(cat ./keys/$SHIP_NAME.pub)\"}" "https://api.digitalocean.com/v2/account/keys")
+    # Parse JSON reponse from key creation to get the key ID
+    key_id=$(echo $key_response | jq -r '.ssh_key.id')
+    # Create a Droplet that will accept connections from the provided SSH key ID
+    curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${DO_TOKEN}" -d "{\"name\":\"${SHIP_NAME}\",\"region\":\"nyc3\",\"size\":\"s-2vcpu-4gb\",\"image\":\"ubuntu-20-04-x64\",\"ssh_keys\":[${key_id}],\"backups\":false,\"ipv6\":true,\"user_data\":null,\"private_networking\":null,\"volumes\": null,\"tags\":[\"urbit\"]}" "https://api.digitalocean.com/v2/droplets" 
 }
+
+create_droplet
